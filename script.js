a1a545c80cadc7afe83eb339957ea4ff2d294fc2
Josefina <lonuz.j@gmail.com>
2016-10-12 15:45:58 -0300

    ejercicio terminando

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // tu codigo va aca
+
+// ejercicio 1
+
+function callbackPromedio (a, b, c, callback){
+	var promedio = (a+b+c)/3;
+
+	return callback(promedio);
+
+}
+
+
+
+// ejercicio 2
+
+var cambioSegundos=1;
+
+function segundero() {
+	document.getElementById("titulo").innerHTML = cambioSegundos++;
+
+}
+setInterval(segundero, 1000);
